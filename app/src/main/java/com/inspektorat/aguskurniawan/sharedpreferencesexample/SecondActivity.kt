04d2e89d75b816c76533ce2383ab7f654f18f3ed
agusk13 <agusk1308@gmail.com>
2025-12-14 12:29:38 +0700

    first commit

---
+++ app/src/main/java/com/inspektorat/aguskurniawan/sharedpreferencesexample/SecondActivity.kt
@@ -0,0 +1,30 @@
+package com.inspektorat.aguskurniawan.sharedpreferencesexample
+
+import android.content.Context
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.inspektorat.aguskurniawan.sharedpreferencesexample.databinding.ActivitySecondBinding
+
+class SecondActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivitySecondBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivitySecondBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.btnLoad.setOnClickListener {
+            val filename = "$packageName-${MainActivity.RPL}"
+            val pref = getSharedPreferences(filename, Context.MODE_PRIVATE)
+            val firstName = pref.getString("firstName", "")
+            val lastName = pref.getString("lastName", "")
+            binding.tvOutput.text = "$firstName $lastName "
+        }
+    }
+
+    override fun onResume() {
+        super.onResume()
+        binding.tvOutput.text = "Click Button Load Data"
+    }
+}
